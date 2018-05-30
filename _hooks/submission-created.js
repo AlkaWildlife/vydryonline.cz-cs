c87aa27e7e79f80872046528866661d308114b48
Filip Zrůst <filip.zrust@sinsir.net>
2018-05-30 18:21:12 +0200

    Change function debug

--- _hooks/submission-created.js
+++ _hooks/submission-created.js
@@ -1,9 +1,11 @@
 exports.handler = function(event, context, callback) {
-  console.log('<<< Event start')
-  console.log(event)
-  console.log('>>> Event end')
-  console.log('<<< Context start')
-  console.log(context)
-  console.log('>>> Context end')
-  callback()
+  setTimeout(function() {
+    console.log('<<< Event start')
+    console.log(event)
+    console.log('>>> Event end')
+    console.log('<<< Context start')
+    console.log(context)
+    console.log('>>> Context end')
+    callback()
+  }, 250)
 }
